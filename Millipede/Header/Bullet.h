a1b75f2545429e3bb4ab6606da3f91f2c233f880
arturo-CE <61763318+arturo-CE@users.noreply.github.com>
2020-04-08 17:15:51 -0700

    Create Bullet.h

---
+++ Millipede/Header/Bullet.h
@@ -0,0 +1,16 @@
+// Created to track the position of the bullet being shot by the character.
+
+#ifndef Bullet_H
+#define Bullet_H
+
+
+typedef struct _Bullet{
+	unsigned char Position;
+	unsigned char Row;
+	unsigned char Matrix;
+	unsigned char Finished_Shooting;
+	
+} Bullet;
+
+
+#endif //Bullet_H
