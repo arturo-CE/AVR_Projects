830f34a5cb3b171126c4b68225f8a92b7f9f8ef1
arturo-CE <61763318+arturo-CE@users.noreply.github.com>
2020-04-08 17:19:05 -0700

    Update io.h

--- Millipede/Header/io.h
+++ Millipede/Header/io.h
@@ -1,1 +1,16 @@
+// Functions used to illuminate the LCD Screen. LCD is used to display score as well as winning and losing screens. 
+#ifndef __io_h__
+#define __io_h__
+
+void LCD_init();
+void LCD_ClearScreen(void);
+void Display_Points(uint8_t hi, uint8_t current);
+void NewHighScore();
+void LCD_WriteCommand (unsigned char Command);
+void LCD_WriteData(unsigned char Data);
+void LCD_Cursor (unsigned char column);
+void LCD_DisplayString(unsigned char column ,const unsigned char *string);
+void Print_Dont_Clear(unsigned char column ,const unsigned char *string);
+void delay_ms(int miliSec);
+#endif
 
