7479d19a532e56c284709b22c85717d4a8a20f2c
arturo-CE <61763318+arturo-CE@users.noreply.github.com>
2020-04-08 17:17:34 -0700

    Create Centipede.h

---
+++ Millipede/Header/Centipede.h
@@ -0,0 +1,15 @@
+// Used to create 4 seperate enemies. Parameters allows us to track the position of each enememy as well as their life status. 
+
+#ifndef CENTIPEDE_H
+#define CENTIPEDE_H
+
+
+typedef struct Centipede{
+	unsigned char Position;
+	unsigned char Direction;
+	unsigned char Row;
+	unsigned char Matrix;
+	
+} Centipede;
+
+#endif //CENTIPEDE_H*/
